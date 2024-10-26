e4c1dc2260d7080f363c91fcffc1976af91f2d8f
Balanagireddy Vallem <53854911+Balu2311@users.noreply.github.com>
2024-10-26 23:29:19 +0530

    Update app_without_sus_stream.py

--- Conversational-AI-API/app_without_sus_stream.py
+++ Conversational-AI-API/app_without_sus_stream.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 # Load environment variables
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
+#openai.api_key = os.getenv('OPENAI_API_KEY')
 openai.api_key = st.secrets["open_ai_key"]
 
 if openai.api_key is None:
